5fd0a77ee97dc7d7367b8cbf9724e3dfb6fe7b04
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2019-03-09 19:34:51 +0100

    Allow detection of English-language 'problem', and match multiple words

--- usun-zadanie.js
+++ usun-zadanie.js
@@ -2,7 +2,7 @@
 // @name         Usuń "Zadanie"
 // @namespace    https://github.com/Aleshkev/sio2-experience-enhancements
 // @downloadURL  https://raw.githubusercontent.com/Aleshkev/sio2-experience-enhancements/master/usun-zadanie.js
-// @version      0.3
+// @version      0.4
 // @description  Usuwa tekst "Zadanie" przed prawdziwą nazwą zadania.
 // @author       Jonas Aleshkevich
 // @match        https://sio2.staszic.waw.pl/*
@@ -17,8 +17,8 @@
   let options = $("*:not(oioioi-message *)");
   options.each(i => {
     let o = options[i];
-    if(o.text != o.innerHTML) return;  // Skip complex elements.
-    o.innerHTML = o.innerHTML.replace(/^zadanie\s+/gi, "");  // Be gone, you ugly piece of garbage!!
+    if (o.text != o.innerHTML) return;  // Skip complex elements.
+    o.innerHTML = o.innerHTML.replace(/^((zadanie|problem)\s+)+/gi, "");  // Be gone, you ugly piece of garbage!!
   });
 
 })();
